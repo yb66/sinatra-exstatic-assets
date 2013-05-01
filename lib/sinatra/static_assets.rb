fbb1907b2721015259fb58a0c8ba96af85797763
Iain Barnett <iainspeed@gmail.com>
2013-05-01 12:24:31 +0100

    Updated for readability.

--- lib/sinatra/static_assets.rb
+++ lib/sinatra/static_assets.rb
@@ -17,39 +17,44 @@ module Sinatra
       # The default value of +closed+ option is +false+.
       #
       def image_tag(source, options = {})
-        options[:src] = url_for(source)
-        tag("img", options)
+        options[:src] = url_for source
+        tag "img", options
       end
 
       def stylesheet_link_tag(*sources)
-        list, options = extract_options(sources)
-        list.collect { |source| stylesheet_tag(source, options) }.join("\n")
+        list, options = extract_options sources
+        list.map {|source|
+          stylesheet_tag source, options
+        }.join "\n"
       end
 
       def javascript_script_tag(*sources)
-        list, options = extract_options(sources)
-        list.collect { |source| javascript_tag(source, options) }.join("\n")
+        list, options = extract_options sources
+        list.map {|source|
+          javascript_tag source, options
+        }.join "\n"
       end
 
       alias :javascript_include_tag :javascript_script_tag
 
       def link_to(desc, url, options = {})
-        tag("a", options.merge(:href => url_for(url))) do
+        tag("a", options.merge(:href => url_for(url))) {
           desc
-        end
+        }
       end
       
       def link_favicon_tag(source = nil, options = {})
         source = "favicon.ico" if source.nil? or source.empty?
-        unless settings.xhtml
-          # html5 style like <link rel="icon" href="http://example.com/myicon.ico" />
-          options[:rel] = options[:rel] || "icon"
-        else
-          # xhtml style like <link rel="shortcut icon" href="http://example.com/myicon.ico" />
-          options[:rel] = "shortcut icon"
-        end
+
+        # xhtml style like <link rel="shortcut icon" href="http://example.com/myicon.ico" />
+        options[:rel] ||= "shortcut icon" if settings.xhtml
+
+        # html5 style like <link rel="icon" href="http://example.com/myicon.ico" />
+        options[:rel] ||= "icon"
+
         options[:href] = url_for(source)
-        tag("link", options)
+        
+        tag "link", options
       end
 
       private
@@ -57,9 +62,18 @@ module Sinatra
       def url_for(addr, absolute = false)
         uri(addr, absolute == :full ? true : false, true)
       end
-      
+
+
+      # TODO change this
       def is_uri?(path)
-          path =~ %r{^[-a-z]+://|^cid:|^//}
+        path =~ %r{\A
+                  [\-a-z]+
+                  ://         # wtf?
+                    |
+                  ^cid:       # wtf?
+                    |
+                  ^//         # again, wtf?
+                \Z}x
       end
 
       def asset_url_for(source)
@@ -67,59 +81,65 @@ module Sinatra
         return url if is_uri?(source)
           
         timestamp = asset_timestamp(source)
-        url += "?#{timestamp}" unless timestamp.empty?
+        url += "?#{timestamp}" if timestamp
         return url
       end
 
       def asset_timestamp(source)
-        if timestamp = @@asset_timestamps_cache[source]
-          return timestamp
+        timestamp = if @@asset_timestamps_cache.has_key? source
+          @@asset_timestamps_cache[source]
         else
           path = asset_file_path(source)
-          timestamp = File.exists?(path) ? File.mtime(path).to_i.to_s : ''
+          timestamp = File.exists?(path) && File.mtime(path).to_i.to_s
           @@asset_mutex.synchronize {
             @@asset_timestamps_cache[source] = timestamp
           }
+          timestamp
         end
       end
 
+
       def asset_file_path(path)
-        File.join(settings.root, 'public', path)
+        File.join(settings.public_folder, path)
       end
 
       def tag(name, local_options = {})
-        start_tag = "<#{name}#{tag_options(local_options) if local_options}"
+        start_tag = "<#{name} #{tag_options(local_options)}".strip
         if block_given?
-          content = yield
-          "#{start_tag}>#{content}</#{name}>"
+          "#{start_tag}>#{yield}</#{name}>"
         else
-          "#{start_tag}#{"/" if settings.xhtml}>"
+          settings.xhtml ? 
+            "#{start_tag}/>" :
+            "#{start_tag}>"
         end
       end
 
       def tag_options(options)
-        unless options.empty?
-          attrs = []
-          #attrs = options.map { |key, value| %(#{key}="#{Rack::Utils.escape_html(value)}") }
-          attrs = options.map { |key, value| %(#{key}="#{value}") }
-          " #{attrs.sort * ' '}" unless attrs.empty?
-        end
+        options.sort
+               .map {|key, value| %(#{key}="#{value}") }
+               .join " "
       end
 
       def stylesheet_tag(source, options = {})
-        tag("link", { :type => "text/css",
-            :charset => "utf-8", :media => "screen", :rel => "stylesheet",
-            :href => asset_url_for(source) }.merge(options))
+        tag "link", 
+            { :type => "text/css",
+              :charset => "utf-8",
+              :media => "screen",
+              :rel => "stylesheet",
+              :href => asset_url_for(source)
+            }.merge(options)
       end
 
       def javascript_tag(source, options = {})
-        tag("script", { :type => "text/javascript", :charset => "utf-8",
-            :src => asset_url_for(source) }.merge(options)) do
-            end
+        tag("script", 
+            { :type => "text/javascript", 
+              :charset => "utf-8",
+              :src => asset_url_for(source)
+            }.merge(options)) {} # empty tag
       end
 
       def extract_options(a)
-        opts = a.last.is_a?(::Hash) ? a.pop : {}
+        opts = a.last.respond_to?(:keys) ? a.pop : {}
         [a, opts]
       end
 
