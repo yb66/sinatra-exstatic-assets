69b5aee25f9ef56fa0dec41eae7a90d554960a0d
Iain Barnett <iainspeed@gmail.com>
2013-05-04 15:33:37 +0100

    Don't need these tests or examples anymore.

--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'turn'
-path = File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
-$:.unshift(path) unless $:.include?(path)
-
-def asset_timestamp(source)
-  File.mtime("./test/public/#{source}").to_i.to_s
-end
