163e5bcf38b46351a48740dac9463e40674dcac0
Iain Barnett <iainspeed@gmail.com>
2013-05-04 15:41:56 +0100

    Mutex wasn't needed, or I couldn't see the point to it; Timestamp now helps with caching via appending to the url; cut down on some repetition; moved private methods into their own module for organisation and testing; better and shorter names for the methods; removed the link_to tag, that's not static stuff; used some objects to encapsulate and organise things, and make them easier to call.

--- lib/sinatra/static_assets.rb
+++ lib/sinatra/static_assets.rb
@@ -2,10 +2,123 @@ require 'sinatra/base'
 
 module Sinatra
   module StaticAssets
-    module Helpers
 
-      @@asset_timestamps_cache = {}
-      @@asset_mutex ||= Mutex.new
+    class Tag < ::String
+      DEFAULT_OPTIONS = {}
+
+      def initialize( name, options={}, &block )
+        @name       = name
+        @closed = (c = options.delete(:closed)).nil? ? true : c
+        @options    = DEFAULT_OPTIONS.merge options
+        @attributes = self.class.make_attributes @options
+        @block      = block
+        super tag
+      end
+
+      attr_reader :attributes, :options, :name
+
+      def tag
+        return @tag if @tag
+        start_tag = "<#{@name} #{@attributes}".strip
+        @tag = if @block
+          "#{start_tag}>#{@block.call}</#{name}>"
+        elsif @closed
+          "#{start_tag} />"
+        else
+          "#{start_tag}>"
+        end
+      end
+
+      def self.make_attributes( options )
+        options.sort
+               .map {|key, value| %(#{key}="#{value}") }
+               .join " "
+      end
+
+    end
+
+    class Href < Tag
+      # TODO change this
+      def is_uri?
+        @options[:href] =~ %r{\A
+                  [\-a-z]+
+                  ://         # wtf?
+                    |
+                  ^cid:       # wtf?
+                    |
+                  ^//         # again, wtf?
+                \Z}x
+      end
+    end
+
+
+    class Asset < ::String
+
+      attr_reader :fullpath
+
+      def initialize( filename, asset_dir=nil ) # TODO failure strategy
+        if asset_dir.nil?
+          filename, asset_dir = [File.basename(filename), File.dirname(filename)]
+        end
+        # TODO fail if asset_dir.nil?
+        super filename
+        @fullpath = File.join asset_dir, filename
+      end
+
+      def timestamp
+        @timestamp ||= File.exists?(fullpath) && File.mtime(fullpath).to_i
+      end
+
+      def querystring
+        timestamp ? "?ts=#{timestamp}" : nil
+      end
+    end
+
+    module Private
+
+      def sss_url_for(addr, options=nil)
+        options ||= {}
+        absolute = options.fetch :absolute, false
+        script_tag = options.fetch :script_tag, true
+        href = uri addr, absolute, script_tag
+        addr.respond_to?(:querystring) ?
+          "#{href}#{addr.querystring}" :
+          href
+      end
+
+      DEFAULT_CSS = {
+#               :type     => "text/css", # not needed with HTML5
+        :charset  => "utf-8",
+        :media    => "screen",
+        :rel      => "stylesheet"
+      }
+
+      def sss_stylesheet_tag(source, options = {})
+        asset_dir = options.delete(:asset_dir) || settings.public_folder
+        asset = Asset.new source, asset_dir
+        href = sss_url_for( asset )
+        Tag.new "link", DEFAULT_CSS.merge(:href => href)
+                                   .merge(options)
+            
+      end
+
+      DEFAULT_JS = {
+#         :type => "text/javascript", 
+        :charset => "utf-8"
+      }
+
+      def sss_javascript_tag(source, options = {})
+        asset = Asset.new source, settings.public_folder
+        href = sss_url_for asset 
+        Tag.new("script", DEFAULT_JS.merge(:src => href)
+                                            .merge(options)          
+        ) {}
+      end
+
+      def sss_extract_options(a)
+        opts = a.last.respond_to?(:keys) ? a.pop : {}
+        [a, opts]
+      end
 
       # In HTML <link> and <img> tags have no end tag.
       # In XHTML, on the contrary, these tags must be properly closed.
@@ -16,132 +129,47 @@ module Sinatra
       #
       # The default value of +closed+ option is +false+.
       #
-      def image_tag(source, options = {})
-        options[:src] = url_for source
-        tag "img", options
+      def sss_image_tag(source, options = {})
+        options[:src] = sss_url_for Asset.new( source, settings.public_folder )
+        Tag.new "img", options
       end
 
-      def stylesheet_link_tag(*sources)
-        list, options = extract_options sources
-        list.map {|source|
-          stylesheet_tag source, options
-        }.join "\n"
+    end
+
+
+    module Helpers
+      include Private
+
+
+      %w{image_tag stylesheet_tag javascript_tag}.each do |method_name|
+        define_method method_name do |*sources|
+          list, options = sss_extract_options sources
+          list.map {|source|
+            send "sss_#{method_name}", source, options
+          }.join "\n"
+        end
       end
 
-      def javascript_script_tag(*sources)
-        list, options = extract_options sources
-        list.map {|source|
-          javascript_tag source, options
-        }.join "\n"
-      end
-
-      alias :javascript_include_tag :javascript_script_tag
-
-      def link_to(desc, url, options = {})
-        tag("a", options.merge(:href => url_for(url))) {
-          desc
-        }
-      end
+      alias_method :img_tag, :image_tag
+      alias_method :css_tag, :stylesheet_tag
+      alias_method :stylesheet, :stylesheet_tag
+      alias_method :javascript_include_tag, :javascript_tag
+      alias_method :js_tag, :javascript_tag
+      alias_method :script_tag, :javascript_tag
       
-      def link_favicon_tag(source = nil, options = {})
+      def favicon_tag(*args)
+        source, options = sss_extract_options args
         source = "favicon.ico" if source.nil? or source.empty?
 
         # xhtml style like <link rel="shortcut icon" href="http://example.com/myicon.ico" />
-        options[:rel] ||= "shortcut icon" if settings.xhtml
+        options[:rel] ||= settings.xhtml ? "shortcut icon" : "icon"
 
-        # html5 style like <link rel="icon" href="http://example.com/myicon.ico" />
-        options[:rel] ||= "icon"
-
-        options[:href] = url_for(source)
+        options[:href] = sss_url_for(source, options.delete(:url_options))
         
-        tag "link", options
+        Tag.new "link", options
       end
 
-      private
-
-      def url_for(addr, absolute = false)
-        uri(addr, absolute == :full ? true : false, true)
-      end
-
-
-      # TODO change this
-      def is_uri?(path)
-        path =~ %r{\A
-                  [\-a-z]+
-                  ://         # wtf?
-                    |
-                  ^cid:       # wtf?
-                    |
-                  ^//         # again, wtf?
-                \Z}x
-      end
-
-      def asset_url_for(source)
-        url = url_for(source)
-        return url if is_uri?(source)
-          
-        timestamp = asset_timestamp(source)
-        url += "?#{timestamp}" if timestamp
-        return url
-      end
-
-      def asset_timestamp(source)
-        timestamp = if @@asset_timestamps_cache.has_key? source
-          @@asset_timestamps_cache[source]
-        else
-          path = asset_file_path(source)
-          timestamp = File.exists?(path) && File.mtime(path).to_i.to_s
-          @@asset_mutex.synchronize {
-            @@asset_timestamps_cache[source] = timestamp
-          }
-          timestamp
-        end
-      end
-
-
-      def asset_file_path(path)
-        File.join(settings.public_folder, path)
-      end
-
-      def tag(name, local_options = {})
-        start_tag = "<#{name} #{tag_options(local_options)}".strip
-        if block_given?
-          "#{start_tag}>#{yield}</#{name}>"
-        else
-          settings.xhtml ? 
-            "#{start_tag}/>" :
-            "#{start_tag}>"
-        end
-      end
-
-      def tag_options(options)
-        options.sort
-               .map {|key, value| %(#{key}="#{value}") }
-               .join " "
-      end
-
-      def stylesheet_tag(source, options = {})
-        tag "link", 
-            { :type => "text/css",
-              :charset => "utf-8",
-              :media => "screen",
-              :rel => "stylesheet",
-              :href => asset_url_for(source)
-            }.merge(options)
-      end
-
-      def javascript_tag(source, options = {})
-        tag("script", 
-            { :type => "text/javascript", 
-              :charset => "utf-8",
-              :src => asset_url_for(source)
-            }.merge(options)) {} # empty tag
-      end
-
-      def extract_options(a)
-        opts = a.last.respond_to?(:keys) ? a.pop : {}
-        [a, opts]
-      end
+      alias_method :link_favicon_tag, :favicon_tag
 
     end
 
