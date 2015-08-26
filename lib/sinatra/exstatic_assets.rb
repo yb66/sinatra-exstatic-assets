bc5a98a2001b565ab0e61191f8d5e4abb1e198c2
Iain Barnett <iainspeed@gmail.com>
2015-08-26 17:46:57 +0100

    Was wrongly useing script_tag instead of script_name, fixed.

--- lib/sinatra/exstatic_assets.rb
+++ lib/sinatra/exstatic_assets.rb
@@ -132,7 +132,7 @@ module Sinatra
       # @param [String,#querystring] addr
       # @param [Hash] options
       # @option options [TrueClass] :absolute see Sinatra::Helpers#uri
-      # @option options [TrueClass] :script_tag Whether to prepend the SCRIPT_TAG env variable.
+      # @option options [TrueClass] :script_name Whether to prepend the SCRIPT_NAME env variable.
       # @return [String]
       # @see Sinatra::Helpers#uri
       def sss_url_for(addr, options=nil)
@@ -163,7 +163,7 @@ module Sinatra
       # @param [Hash] options
       # @option options [String] :asset_dir The directory the asset is held. Defaults to Sinatra's `public_folder` setting.
       # @option options [Hash] :url_options Options for devising the URL.
-      # @option options [TrueClass] :script_tag Whether to prepend the SCRIPT_TAG env variable.
+      # @option options [TrueClass] :script_name Whether to prepend the SCRIPT_NAME env variable.
       # @return [Tag]
       def sss_stylesheet_tag(source, options = {})
         asset_dir = options.delete(:asset_dir) || settings.public_folder
@@ -276,7 +276,7 @@ module Sinatra
 
       # @param [String] source
       # @param [Hash] options
-      # @option options [Hash] :url_options script_tag
+      # @option options [Hash] :url_options script_name
       # @example
       #   favicon_tag
       #   # => <link href="/favicon.ico" rel="icon">
